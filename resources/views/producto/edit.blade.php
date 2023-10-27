58d5e51c99bc7c573ee60c766a64153b30bb05a2
Jeferson Soria <soriaarandajeferson@gmail.com>
2023-10-27 09:26:47 -0500

    agregando nac

--- resources/views/producto/edit.blade.php
+++ resources/views/producto/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-dark-800 dark:text-dark-200 leading-tight">
             {{ __('Editar') }}
         </h2>
     </x-slot>
